dac3c59dd70070c7237957f943c6d0e9ed1ab883
leonraz58 <leonraz58@gmail.com>
2024-01-15 22:53:44 +0300

    hw11 completed

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -17,6 +17,13 @@ function HW11() {
 
     const change = (event: any, value: any) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        }
+        if (typeof value === "number") {
+            setValue1(value)
+        }
     }
 
     return (
@@ -30,6 +37,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
+                            value={value1}
+                            onChange={change}
 
                         />
                     </div>
@@ -38,6 +47,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
+                            value={[value1, value2]}
+                            onChange={change}
 
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,11 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                width: 150,
+                color: '#0C2',
+                getAriaValueText: '#8B8B8B',
+                margin: '10px'
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
